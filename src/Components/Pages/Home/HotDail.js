9198111256e1f1f4bd533799c1c233ab799b484e
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-26 12:11:43 +0600

    hot dail polish

--- src/Components/Pages/Home/HotDail.js
+++ src/Components/Pages/Home/HotDail.js
@@ -3,13 +3,22 @@ import hotImage from "../../../Images/Hot dails/Important-TollFree-Numbers.png";
 
 const HotDail = () => {
   return (
-    <div className="flex justify-center mt-7">
-      <img
-        style={{ height: "600px" }}
-        className="rounded-xl shadow-inner"
-        src={hotImage}
-        alt=""
-      />
+    <div
+      style={{
+        background: `url(https://mlawiy0je0ms.i.optimole.com/206F41w.2d6g.2d53d/w:1800/h:1012/q:auto/https://www.planetwatch.io/wp-content/uploads/2022/08/Air-pollution.jpg)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className="h-screen"
+    >
+      {' '}
+      <div className="flex justify-center pt-20">
+        <img
+          className="rounded-xl shadow-inner h-[600px] w-[500px]"
+          src={hotImage}
+          alt=""
+        />
+      </div>
     </div>
   );
 };
