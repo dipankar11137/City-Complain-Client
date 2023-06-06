26657c4fb3f433ef4aaac51d73ba8f86199c9942
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-07 00:40:55 +0600

    edit show complain

--- src/Components/Pages/Home/ShowComplain.js
+++ src/Components/Pages/Home/ShowComplain.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const ShowComplain = ({ complain }) => {
-  // console.log(complain);
+  console.log(complain);
   return (
     <div>
       <div
-        style={{ width: "" }}
+        style={{ width: '' }}
         className="card bg-base-100 w-96 text-black shadow-2xl hover:bg-red-100  hover:shadow-inner"
       >
         <figure>
@@ -13,39 +13,44 @@ const ShowComplain = ({ complain }) => {
             className="w-full pic-style"
             src={complain?.image}
             alt=""
-            style={{ height: "350px" }}
+            style={{ height: '350px' }}
           />
         </figure>
         <div className="card-body">
           <h2 className="card-title  text-lg">
-            {" "}
+            {' '}
             <span className="font-bold"> Division :</span>
             {complain?.division}
           </h2>
           <h2 className="card-title text-lg font-medium ">
-            {" "}
+            {' '}
             <span className="font-bold"> District : </span>
             {complain?.district}
           </h2>
           <h2 className="card-title  text-sm font-medium">
-            {" "}
+            {' '}
             <span className="font-bold text-lg"> Ministry : </span>
             {complain?.ministry}
           </h2>
           <h2 className="card-title  text-sm font-medium">
-            {" "}
+            {' '}
             <span className="font-bold text-lg"> Current Location : </span>
             {complain?.location}
           </h2>
           <h2 className="card-title  text-sm font-medium">
-            {" "}
+            {' '}
             <span className="font-bold text-lg"> Date : </span>
             {complain?.date}
           </h2>
           <h2 className="card-title  text-sm font-medium">
-            {" "}
+            {' '}
             <span className="font-bold text-lg"> Complain : </span>
             {complain?.complain}
+          </h2>
+          <h2 className="card-title  text-sm font-medium">
+            {' '}
+            <span className="font-bold text-lg"> Complain by : </span>
+            {complain?.profile?.name}
           </h2>
         </div>
       </div>
